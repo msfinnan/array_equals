788a2ca4bb5d6a5fa23e3d59ae83b33e7fa3feb9
Margaret Finnan <msfinnan@gmail.com>
2019-04-21 17:53:09 -0700

    redid assignment without using ruby built in methods

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,13 +2,35 @@
 # and the same integer values in the same exact order
 
 def array_equals(array1, array2)
-  if (array1 == nil) && (array2 == nil)
+  if array1 == nil && array2 == nil
     return true
-  elsif array1 == nil || array2 == nil
-    return false
-  elsif (array1.length == array2.length) && (array1[0..-1] == array2[0..-1])
-    return true
-  else
+  end
+  if array1 == nil || array2 == nil
     return false
   end
+  if array1.length != array2.length
+    return false
+  end
+
+  i = 0
+  while i < array1.length
+    if array1[i] != array2[i]
+      return false
+    end
+    i += 1
+  end
+
+  return true
 end
+
+# def array_equals(array1, array2)
+#   if (array1 == nil) && (array2 == nil)
+#     return true
+#   elsif array1 == nil || array2 == nil
+#     return false
+#   elsif (array1.length == array2.length) && (array1[0..-1] == array2[0..-1])
+#     return true
+#   else
+#     return false
+#   end
+# end
